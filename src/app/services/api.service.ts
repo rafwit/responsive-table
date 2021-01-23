187582f2816aa1b27386b8315c33aa4dfaa54454
Rafal Witczak <rwitczak89@gmail.com>
2021-01-23 21:56:57 +0100

    refactor(api-service): rename product service >> api service and move to service/ dir

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,22 +1,18 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Product } from './product';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Product } from '../product';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductsService {
+export class ApiService {
   private BASE_URL = environment.products_api_url;
 
   constructor(public http: HttpClient) {}
 
   getProducts(start: number): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.BASE_URL}${start}`);
-  }
-
-  calculateWhichProductsToFetch(currentPage: number) {
-    return currentPage * 50;
   }
 }
