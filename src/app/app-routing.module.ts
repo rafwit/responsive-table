f2c5d20cdaab9543ecd26f43461b5af0950c02fb
Rafal Witczak <rwitczak89@gmail.com>
2021-01-24 13:35:43 +0100

    feat(global): finish pagination
    routes to new page and scrolls to the top onInit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'list/:pageNumber', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from '../product';
 import { ApiService } from '../services/api.service';
+import { ScrollTopService } from '../services/scroll-top.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   updatedProductsList: Product[] = [];
   show = false;
 
-  constructor(private apiService: ApiService) {}
+  productSubscription: Subscription = new Subscription();
+
+  constructor(private apiService: ApiService, private scrollToTopService: ScrollTopService) {}
 
   receiveUpdatedProductList(data: Product[]) {
     this.updatedProductsList = data;
   }
 
   ngOnInit(): void {
-    this.apiService.getProducts(0).subscribe((data: Product[]) => {
-      console.log(data);
+    this.productSubscription = this.apiService.getProducts(0).subscribe((data: Product[]) => {
       this.updatedProductsList = data;
+      this.scrollToTopService.setScrollTop();
       this.show = true;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.productSubscription.unsubscribe();
   }
 }
