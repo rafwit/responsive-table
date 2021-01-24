71e0a4d3148410d5cff0766cc0e904eb301a2773
Rafal Witczak <rwitczak89@gmail.com>
2021-01-24 13:34:47 +0100

    feat: finish navigation bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { Event } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 import { Product } from '../product';
 import { ApiService } from '../services/api.service';
 
@@ -8,30 +9,33 @@ import { ApiService } from '../services/api.service';
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.scss'],
 })
-export class NavigationBarComponent {
+export class NavigationBarComponent implements OnDestroy {
   @Output() updateProductsList = new EventEmitter();
+  productSubscription: Subscription = new Subscription();
 
-  constructor(private apiService: ApiService) {}
   currentPage = 1;
 
-  calculateWhichProductsToFetch(currentPage: number) {
-    if (currentPage === 1) return 1;
-    return currentPage * 50;
-  }
+  constructor(private apiService: ApiService) {}
 
   showNextPage() {
     const start = this.currentPage * 50;
-    this.apiService.getProducts(start).subscribe((data: Product[]) => {
-      this.updateProductsList.emit(data);
-    });
+    this.productSubscription = this.fillPageWithProducts(start);
     this.currentPage += 1;
   }
 
   showPreviousPage() {
     const start = (this.currentPage - 2) * 50;
-    this.apiService.getProducts(start).subscribe((data: Product[]) => {
+    this.productSubscription = this.fillPageWithProducts(start);
+    this.currentPage -= 1;
+  }
+
+  fillPageWithProducts(start: number): Subscription {
+    return this.apiService.getProducts(start).subscribe((data: Product[]) => {
       this.updateProductsList.emit(data);
     });
-    this.currentPage -= 1;
+  }
+
+  ngOnDestroy(): void {
+    this.productSubscription.unsubscribe();
   }
 }
