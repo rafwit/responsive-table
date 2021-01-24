8c1aec9756d2ebade7644d7741dcc2251e3a0027
Rafal Witczak <rwitczak89@gmail.com>
2021-01-24 13:34:20 +0100

    feat: add scrollToTop service
    to make sure that after changing page, there is scroll to the top happening

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ApiService } from './services/api.service';
+import { BarRatingModule } from 'ngx-bar-rating';
+import { ScrollTopService } from './services/scroll-top.service';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent, NavigationBarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [ApiService],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BarRatingModule],
+  providers: [ApiService, ScrollTopService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
