24264174e520c1d74b0d1c87eb64d1a8c42737d3
Rafal Witczak <rwitczak89@gmail.com>
2021-01-24 14:26:21 +0100

    feat(global): add fixed header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { ApiService } from './services/api.service';
 import { BarRatingModule } from 'ngx-bar-rating';
 import { ScrollTopService } from './services/scroll-top.service';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, NavigationBarComponent],
+  declarations: [AppComponent, ProductListComponent, NavigationBarComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BarRatingModule],
   providers: [ApiService, ScrollTopService],
   bootstrap: [AppComponent],
